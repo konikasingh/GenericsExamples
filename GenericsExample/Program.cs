e384c1df9585b50e75a0e1a8d11843acd8a07579
konikasingh <konikasingh1996@gmail.com>
2021-10-25 16:06:04 +0530

    String Program in Generics

--- GenericsExample/Program.cs
+++ GenericsExample/Program.cs
@@ -6,18 +6,18 @@ namespace GenericsExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Enter the First number:");
-            float f = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Enter the First string:");
+            string f = Console.ReadLine();
 
-            Console.WriteLine("Enter the second number:");
-            float s = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Enter the second string:");
+            string s = Console.ReadLine();
 
-            Console.WriteLine("Enter the third number:");
-            float t = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Enter the third string:");
+            string t = Console.ReadLine();
 
             SimpleClass simple = new SimpleClass(f, s, t);
             simple.findMax();
+
         }
     }
 }
