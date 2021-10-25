cca8bcdca7b6f0731a0d62ab13b95ca1e85ae949
konikasingh <konikasingh1996@gmail.com>
2021-10-25 15:15:05 +0530

    Integer example added

--- GenericsExample/Program.cs
+++ GenericsExample/Program.cs
@@ -5,8 +5,18 @@ namespace GenericsExample
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Generics World");
+        {       
+            Console.WriteLine("Enter the First number:");
+            int f = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int s = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number:");
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            SimpleClass simple = new SimpleClass(f, s, t);
+            simple.findMax();
         }
     }
 }
