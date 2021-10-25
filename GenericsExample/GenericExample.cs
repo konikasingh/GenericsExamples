4aae4a24903408a041bc45f894eee3c8fb6c2189
konikasingh <konikasingh1996@gmail.com>
2021-10-25 18:44:47 +0530

    Extend Program Using Generic Method

--- GenericsExample/GenericExample.cs
+++ GenericsExample/GenericExample.cs
@@ -8,28 +8,38 @@ namespace GenericsExample
 {
     class GenericMaxNumber<T> where T : IComparable
     {
+        /// <summary>
+        /// finding max value in array and sorting them using sort function
+        /// </summary>
         public T[] Value;
 
         public GenericMaxNumber(T[] value)
         {
             this.Value = value;
         }
-        public T maxValue(params T[] values)
+        public T[] Sort1(T[] values)
         {
-
+            Array.Sort(values);
+            Console.WriteLine("the sorted array is :");
             foreach (var i in values)
             {
                 Console.WriteLine(i);
-
             }
-            return default;
+            return values;
+        }
+        public T maxValue(params T[] values)
+        {
+
+            var sorted_values = Sort1(values);
+            return sorted_values[^1];
 
         }
 
         public void printMaxValue()
         {
             var max = maxValue(this.Value);
-        }
+            Console.WriteLine("Maximum value is:" + max);
 
+        }
     }
 }
