dac742206bddd0cb76c2ae217529ea87425ac3f8
konikasingh <konikasingh1996@gmail.com>
2021-10-25 16:31:06 +0530

    Extended Program

---
+++ GenericsExample/GenericExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsExample
+{
+    class GenericMaxNumber<T> where T : IComparable
+    {
+        public T[] Value;
+
+        public GenericMaxNumber(T[] value)
+        {
+            this.Value = value;
+        }
+        public T maxValue(params T[] values)
+        {
+
+            foreach (var i in values)
+            {
+                Console.WriteLine(i);
+
+            }
+            return default;
+
+        }
+
+        public void printMaxValue()
+        {
+            var max = maxValue(this.Value);
+        }
+
+    }
+}
--- GenericsExample/Program.cs
+++ GenericsExample/Program.cs
@@ -6,17 +6,31 @@ namespace GenericsExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the First string:");
-            string f = Console.ReadLine();
+            Console.WriteLine("Hello World!");
+            int[] arr = { 112, 342, 654, 567, 897 };
 
-            Console.WriteLine("Enter the second string:");
-            string s = Console.ReadLine();
+            GenericMaxNumber<int> simple = new GenericMaxNumber<int>(arr);
+            simple.printMaxValue();
 
-            Console.WriteLine("Enter the third string:");
-            string t = Console.ReadLine();
 
-            SimpleClass simple = new SimpleClass(f, s, t);
-            simple.findMax();
+
+
+
+            /*GenericMaxNumber<float> simple1 = new GenericMaxNumber<float>(25.20f, 25.33f, 25.12f);
+           simple1.findMax1();
+           GenericMaxNumber<string> simple2 = new GenericMaxNumber<string>("abc", "red", "wsx");
+           simple.findMax1();
+           Console.ReadLine();*/
+
+            /*Console.WriteLine("Enter the First number:");
+            int f = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number:");
+            int t = Convert.ToInt32(Console.ReadLine());*/
+
+
+
 
         }
     }
